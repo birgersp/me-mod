aa9c35fe342c39d2cf91c9a64e15cedf1994d944
Birger Skogeng Pedersen <birgersp@gmail.com>
2019-03-22 20:01:53 +0100

    initial

---
+++ Data/Scripts/BirgerMod.cs
@@ -0,0 +1,49 @@
+﻿using Medieval.Entities.Components.Blocks;
+using Sandbox.Game.Gui;
+using Sandbox.ModAPI;
+using VRage.Components;
+using VRage.Game.Input;
+using VRage.Game.ModAPI;
+using VRage.Network;
+using VRage.Session;
+using VRage.Utils;
+using VRageMath;
+
+namespace BirgerMod
+{
+    [MySessionComponent(AlwaysOn = true)]
+    public class BirgerMod : MySessionComponent, IMyEventProxy
+    {
+        MyInputContext m_inputContext = new MyInputContext("ExampleInputContext");
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+            m_inputContext.RegisterAction(MyStringHash.GetOrCompute("ExampleControlName"), HandleInput);
+            m_inputContext.Push();
+        }
+
+        protected override void OnUnload()
+        {
+            m_inputContext.Pop();
+            base.OnUnload();
+        }
+
+        private void HandleInput(ref MyInputContext.ActionEvent action)
+        {
+            MyMultiplayer.RaiseEvent(this, x => x.ServerMethodInvokedByClient);
+        }
+
+        [Event, Reliable, Server]
+        private void ServerMethodInvokedByClient()
+        {
+            MyHud.Notifications.Add(new MyHudNotificationDebug("Message called by client!"));
+            log("Hello world!");
+        }
+
+        private void log(string msg)
+        {
+            ((IMyUtilities)MyAPIUtilities.Static).ShowNotification(msg, 1000, null, Color.White);
+        }
+    }
+}
---
+++ metadata.mod
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ModMetadata xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <ModId>birgermod2</ModId>
+  <ModVersion>1.0</ModVersion>
+  <MinGameVersion>0.5.1</MinGameVersion>
+  <MaxGameVersion>0.8.0</MaxGameVersion>
+</ModMetadata>
