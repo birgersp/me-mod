d26a4dca781317cb646d627be36f05d56686f2fa
Birger Skogeng Pedersen <birgersp@gmail.com>
2019-03-22 22:56:38 +0100

    cleanup in SpawnBot

--- ToughNights/mod/Data/Scripts/ToughNights.cs
+++ ToughNights/mod/Data/Scripts/ToughNights.cs
@@ -2,6 +2,7 @@
 using Sandbox.Game.Entities;
 using Sandbox.Game.Gui;
 using Sandbox.Game.Players;
+using System;
 using VRage;
 using VRage.Game;
 using VRage.Game.Input;
@@ -46,6 +47,18 @@ namespace ToughNights
         [Event, Reliable, Server]
         private void ServerMethodInvokedByClient()
         {
+            try
+            {
+                tryFunction();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        private void tryFunction()
+        {
             var players = MyPlayers.Static.GetAllPlayers();
             var id = new MyDefinitionId(typeof(MyObjectBuilder_HumanoidBot), "BarbarianForestClubStudded");
             foreach (MyPlayer player in players.Values)
@@ -57,50 +70,16 @@ namespace ToughNights
 
         public void SpawnBot(MyDefinitionId botId, Vector3D position)
         {
-
-            //Something along the likes of
-            //MyDefinitionId definitionId = default;
-            //Vector3D worldPosition = Vector3D.Zero;
-
-            //var createdEntity = MySession.Static.Scene.CreateEntity(definitionId);
-            //createdEntity.PositionComp.SetWorldMatrix(MatrixD.CreateWorld(worldPosition));
-            //MySession.Static.Scene.ActivateEntity(createdEntity);
-
-            var botDefinition = (MyAgentDefinition)MyDefinitionManager.Get<MyBotDefinition>(botId);
             var newPos = MyEntities.FindFreePlace(position, 1f, 200, 5, 0.5f);
             if (!newPos.HasValue)
-                MyEntities.FindFreePlace(position, 1f, 200, 5, 5f);
-            if (newPos.HasValue)
-                position = newPos.Value;
-
-            var gravity = VRage.Entities.Gravity.MyGravityProviderSystem.CalculateTotalGravityInPoint(position);
-            if (!Vector3.IsZero(gravity))
-                gravity.Normalize();
-            else
-                gravity = Vector3.Down;
-
-            var spawnWorldMatrix = MatrixD.CreateWorld(position, Vector3.Forward, -gravity);
-            var botEntityOb = (MyObjectBuilder_EntityBase)MyObjectBuilderSerializer.CreateNewObject(botDefinition.BotEntity);
-            botEntityOb.PositionAndOrientation = new MyPositionAndOrientation(spawnWorldMatrix);
-            botEntityOb.PersistentFlags |= MyPersistentEntityFlags2.InScene;
-            var botEntity = MyEntities.CreateFromObjectBuilder(botEntityOb);
-            botDefinition.RaiseBeforeBotSpawned(botEntity);
-
-            // Try without first, uncomment if it doesn't work
-            //            var botName = GetRandomCharacterName();
-            //            var botIdentity = MyIdentities.Static.CreateIdentity(botName);
-            //            MyIdentities.Static.SetControlledEntity(botIdentity, botEntity);
-
-            MyEntities.Add(botEntity);
-
-            botDefinition.AfterBotSpawned(botEntity);
-
-            // may cause issues in MP
-            //            var eventProxyOwner = MyExternalReplicable.FindByObject(botEntity);
-            //            if (eventProxyOwner != null)
-            //                MyMultiplayer.ReplicateImmediately(eventProxyOwner);
-
-            //log("BOT SPAWNED");
+                newPos = MyEntities.FindFreePlace(position, 1f, 200, 5, 5f);
+            var botPosition = newPos.Value;
+            var botDefinition = (MyAgentDefinition)MyDefinitionManager.Get<MyBotDefinition>(botId);
+            var createdEntity = MySession.Static.Scene.CreateEntity(botDefinition.BotEntity);
+            createdEntity.PositionComp.SetWorldMatrix(MatrixD.CreateWorld(botPosition));
+            botDefinition.RaiseBeforeBotSpawned(createdEntity);
+            MySession.Static.Scene.ActivateEntity(createdEntity);
+            botDefinition.AfterBotSpawned(createdEntity);
         }
     }
 }
--- ToughNights/mod/metadata.mod
+++ ToughNights/mod/metadata.mod
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="utf-8"?>
 <ModMetadata xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
   <ModId>toughnights</ModId>
-  <WorkshopId>1690534441</WorkshopId>
+  <WorkshopId>1690593906</WorkshopId>
   <ModVersion>0.1</ModVersion>
   <MinGameVersion>0.7</MinGameVersion>
   <MaxGameVersion>0.8</MaxGameVersion>
