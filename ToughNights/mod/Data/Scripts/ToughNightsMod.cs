5eac397da4511f38cbfec8e56b65d13562b50a93
Birger Skogeng Pedersen <birgersp@gmail.com>
2019-03-23 09:13:50 +0100

    add features

--- ToughNights/mod/Data/Scripts/ToughNightsMod.cs
+++ ToughNights/mod/Data/Scripts/ToughNightsMod.cs
@@ -2,8 +2,12 @@
 using Sandbox.Game.Entities;
 using Sandbox.Game.Gui;
 using Sandbox.Game.Players;
+using Sandbox.Game.SessionComponents;
+using Sandbox.ModAPI;
 using System;
+using System.Collections.Generic;
 using VRage;
+using VRage.Components;
 using VRage.Game;
 using VRage.Game.Input;
 using VRage.Game.ObjectBuilders.AI.Bot;
@@ -16,10 +20,18 @@ using VRageMath;
 namespace ToughNights
 {
     [StaticEventOwner]
+    [MyDependency(typeof(MySectorWeatherComponent))]
     [MySessionComponent(AlwaysOn = true)]
     public class ToughNightsMod : MySessionComponent, IMyEventProxy
     {
         MyInputContext m_inputContext = new MyInputContext("ExampleInputContext");
+        private readonly Dictionary<MyPlayer.PlayerId, Double> playerTargetTimestamps = new Dictionary<MyPlayer.PlayerId, Double>();
+
+        [Automatic]
+        private readonly MySectorWeatherComponent weather = null;
+
+        private Double currentTime_sec;
+        private Double prevUpdateTime_sec;
 
         protected override void OnLoad()
         {
@@ -68,7 +80,43 @@ namespace ToughNights
             }
         }
 
-        public void SpawnBot(MyDefinitionId botId, Vector3D position)
+        [FixedUpdate]
+        private void Update()
+        {
+            if (!MyMultiplayerModApi.Static.IsServer)
+                return;
+
+            currentTime_sec = weather.CurrentTime.TotalSeconds;
+            if (currentTime_sec - prevUpdateTime_sec < 5)
+                return;
+
+            prevUpdateTime_sec = currentTime_sec;
+            log("this is your server!");
+
+            var players = MyPlayers.Static.GetAllPlayers();
+            foreach (MyPlayer player in players.Values)
+            {
+                processPlayer(player);
+            }
+        }
+
+        private void processPlayer(MyPlayer player)
+        {
+            Double timestamp;
+            if (!playerTargetTimestamps.TryGetValue(player.Id, out timestamp))
+            {
+                timestamp = createTargetTimestamp();
+            }
+
+
+        }
+
+        private Double createTargetTimestamp()
+        {
+            return 0;
+        }
+
+        private void SpawnBot(MyDefinitionId botId, Vector3D position)
         {
             var newPos = MyEntities.FindFreePlace(position, 1f, 200, 5, 0.5f);
             if (!newPos.HasValue)
